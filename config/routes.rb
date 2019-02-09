2f7744ec4b668ec3db169882b86fc969261f8c4a
sunakan <sunakan03@gmail.com>
2019-02-09 16:00:43 +0900

    [Change] rootページを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "top/index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "top#index"
 end
