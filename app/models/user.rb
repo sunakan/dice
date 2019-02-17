109ef4315f7b520b984359fa4eaa22e1dc77e483
sunakan <sunakan03@gmail.com>
2019-02-17 18:58:57 +0900

    [Add] 簡単なUserモデルを追加
    - rails cからUser.newで作成可能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name,  type: String
+  field :email, type: String
+  field :password, type: String
+
+  validates_presence_of :email, message: "Eメールアドレスを入力してください"
+  validates_uniqueness_of :email, message: "このEメールアドレスは既に使われています"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "email属性" do
+    context "Eメールアドレスがnil" do
+      it "validationが通らない" do
+        user = User.new(name: "test", email: nil, password: "test")
+        expect(user).not_to be_valid
+      end
+    end
+    context "Eメールアドレスが重複" do
+      it "validationが通らない" do
+        User.create(name: "test", email: "test@example.com", password: "test")
+        user = User.new(name: "test", email: "test@example.com", password: "test")
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
