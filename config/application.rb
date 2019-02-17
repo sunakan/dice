87e02356cd7bd0bad00cbd29d714da6a88967f2e
sunakan <sunakan03@gmail.com>
2019-02-17 17:17:48 +0900

    [Add] railsとmongodbとをmongoidでつなぐ

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.generators do |g|
+      g.orm :mongoid
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
