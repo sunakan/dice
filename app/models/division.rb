198116cf83d4dab62e844893930f7b9cbaece15b
sunakan <sunakan03@gmail.com>
2019-02-23 22:37:42 +0900

    [Clean] rubocop -R
    - required => optional
    - inverse_ofをつけろとのこと

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,4 +1,6 @@
 class Division < ApplicationRecord
+  has_many :members, class_name: "Member", foreign_key: "division_id", inverse_of: :division, dependent: :nullify
+
   validates :division_id, presence: { message: "nilは許可しません" }
   validates :division_id, uniqueness: { message: "重複は許可しません" }
   validates :division_name, presence: { message: "nilは許可しません" }
