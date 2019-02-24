5be60e932230fbe1590dbf1f406242fb609cf037
sunakan <sunakan03@gmail.com>
2019-02-24 21:15:01 +0900

    [Fix] test用DB作成項目をset_up.shに記述

--- setup_db.sh
+++ setup_db.sh
@@ -7,7 +7,9 @@ echo "====================ridgepole -c config/database.yml -E development --appl
 sudo docker-compose exec app bundle exec ridgepole -c config/database.yml -E development --apply -f db/Schemafile
 echo "====================ridgepole -c config/database.yml -E test --apply -f db/Schemafile"
 sudo docker-compose exec app bundle exec ridgepole -c config/database.yml -E test --apply -f db/Schemafile
-echo "====================rake ext_db:create"
-sudo docker-compose exec app bundle exec rake ext_db:create
+echo "====================rake ext_db:create RAILS_ENV=development"
+sudo docker-compose exec app bundle exec rake ext_db:create RAILS_ENV=development
+echo "====================rake ext_db:create RAILS_ENV=test"
+sudo docker-compose exec app bundle exec rake ext_db:create RAILS_ENV=test
 echo "====================rake ext_db:ridgepole:apply"
 sudo docker-compose exec app bundle exec rake ext_db:ridgepole:apply
