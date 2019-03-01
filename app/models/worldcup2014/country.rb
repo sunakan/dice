a3bf1907e166bad4843c4d38bb2ab6e0c9d746c9
sunakan <sunakan03@gmail.com>
2019-03-01 23:12:42 +0900

    [Add] Worldcup2014::Countryモデルのスペックが通るようにする

--- app/models/worldcup2014/country.rb
+++ app/models/worldcup2014/country.rb
@@ -1,2 +1,25 @@
 class Worldcup2014::Country < Worldcup2014::AppRecord
+  module Group
+    A = "A".freeze
+    B = "B".freeze
+    C = "C".freeze
+    D = "D".freeze
+    E = "E".freeze
+    F = "F".freeze
+    G = "G".freeze
+    H = "H".freeze
+    def self.all
+      self.constants.map {|group| eval(group.to_s) } # rubocop:disable Security/Eval
+    end
+  end
+
+  validates :id, presence: true, on: :update
+  validates :id, uniqueness: true, on: :update
+  validates :name, presence: true
+  validates :name, length: { in: 1..50 }
+  validates :ranking, presence: true
+  validates :ranking, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :group_name, presence: true
+  validates :group_name, length: { is: 1 }
+  validates :group_name, inclusion: { in: Group.all }
 end
