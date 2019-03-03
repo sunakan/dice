0ce6a0dc97cfa7593a1e50d301a1c8d6eaefc9be
sunakan <sunakan03@gmail.com>
2019-03-03 13:11:44 +0900

    [Add] Worldcup2014::Goalのテストを通るようにする

--- app/models/worldcup2014/goal.rb
+++ app/models/worldcup2014/goal.rb
@@ -1,2 +1,9 @@
 class Worldcup2014::Goal < Worldcup2014::AppRecord
+  validates :id, presence: true, on: :update
+  validates :id, uniqueness: true, on: :update
+  validates :pairing_id, presence: true
+  validates :player_id, presence: true
+
+  belongs_to :player
+  belongs_to :pairing
 end
