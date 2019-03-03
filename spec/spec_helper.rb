605a88b116528bf211b624a7d45b9a60f11a2400
sunakan <sunakan03@gmail.com>
2019-03-03 16:57:28 +0900

    [Clean] spec/*_helperの改修
    - spec_helper.rbを元々のやつに戻す
    - xxx_helperを分割 => rails_helper.rbでまとめる

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,37 +1,9 @@
-require "simplecov"
-
-SimpleCov.start
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  # :suite => RSpecの実行時に1度だけ実行
-  # :each  => 各example毎に実行
-  # 順番
-  # before :suite
-  # before :context
-  # before :example
-  # after  :example
-  # after  :context
-  # after  :suite
-  config.before(:suite) do
-    DatabaseCleaner[:mongoid].strategy = :truncation
-    DatabaseCleaner[:active_record].strategy = :truncation
-  end
-  config.before do
-    DatabaseCleaner[:mongoid].start
-    DatabaseCleaner[:active_record].start
-  end
-  config.after do
-    DatabaseCleaner[:mongoid].clean
-    DatabaseCleaner[:active_record].clean
-  end
 end
