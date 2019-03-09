ba4c876dc529c54d13a86443bbcd80c5f81a98bf
sunakan <sunakan03@gmail.com>
2019-03-10 08:38:09 +0900

    [Add] #45 DollarとFrancの重複を消すためにRSpecからDollar.newを消す

--- spec/lib/tdd/money_test.rb
+++ spec/lib/tdd/money_test.rb
@@ -3,16 +3,16 @@ require "rails_helper"
 RSpec.describe "Money Test" do # rubocop:disable RSpec/DescribeClass
   it "test multiplication" do
     five = Tdd::Money.dollar(5)
-    expect(five.times(2)).to eql(Tdd::Dollar.new(10))
-    expect(five.times(3)).to eql(Tdd::Dollar.new(15))
+    expect(five.times(2)).to eql(Tdd::Money.dollar(10))
+    expect(five.times(3)).to eql(Tdd::Money.dollar(15))
   end
 
   it "test equality" do
-    expect(Tdd::Dollar.new(5).eql?(Tdd::Dollar.new(5))).to be_truthy
-    expect(Tdd::Dollar.new(5).eql?(Tdd::Dollar.new(6))).to be_falsy
+    expect(Tdd::Money.dollar(5).eql?(Tdd::Money.dollar(5))).to be_truthy
+    expect(Tdd::Money.dollar(5).eql?(Tdd::Money.dollar(6))).to be_falsy
     expect(Tdd::Franc.new(5).eql?(Tdd::Franc.new(5))).to be_truthy
     expect(Tdd::Franc.new(5).eql?(Tdd::Franc.new(6))).to be_falsy
-    expect(Tdd::Dollar.new(5).eql?(Tdd::Franc.new(5))).to be_falsy
+    expect(Tdd::Money.dollar(5).eql?(Tdd::Franc.new(5))).to be_falsy
   end
 
   it "test franc multiplication" do
