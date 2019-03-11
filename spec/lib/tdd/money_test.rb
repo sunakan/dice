4c043bac36aa8240698f1873526d3da8fb554eda
sunakan <sunakan03@gmail.com>
2019-03-11 21:37:15 +0900

    [Clean] テスト名を自分なりに解釈して日本語化
    - スイスフランと米ドルの掛け算はまとめる

--- spec/lib/tdd/money_test.rb
+++ spec/lib/tdd/money_test.rb
@@ -1,30 +1,27 @@
 require "rails_helper"
 
-RSpec.describe "Money Test" do # rubocop:disable RSpec/DescribeClass
-  it "test multiplication" do
+RSpec.describe Tdd::Money do
+  it "乗算" do
     five = Tdd::Money.dollar(5)
     expect(five.times(2)).to eql(Tdd::Money.dollar(10))
     expect(five.times(3)).to eql(Tdd::Money.dollar(15))
-  end
-
-  it "test equality" do
-    expect(Tdd::Money.dollar(5)).to be_eql(Tdd::Money.dollar(5))
-    expect(Tdd::Money.dollar(5)).not_to be_eql(Tdd::Money.dollar(6))
-    expect(Tdd::Money.dollar(5)).not_to be_eql(Tdd::Money.franc(5))
-  end
-
-  it "test franc multiplication" do
     five = Tdd::Money.franc(5)
     expect(five.times(2)).to eql(Tdd::Money.franc(10))
     expect(five.times(3)).to eql(Tdd::Money.franc(15))
   end
 
-  it "test currency" do
+  it "等価性" do
+    expect(Tdd::Money.dollar(5)).to be_eql(Tdd::Money.dollar(5))
+    expect(Tdd::Money.dollar(5)).not_to be_eql(Tdd::Money.dollar(6))
+    expect(Tdd::Money.dollar(5)).not_to be_eql(Tdd::Money.franc(5))
+  end
+
+  it "ファクトリメソッドから生成されたインスタンスの通貨名" do
     expect(Tdd::Money.dollar(1).currency).to eq("USD")
     expect(Tdd::Money.franc(1).currency).to eq("CHF")
   end
 
-  it "test simple addition" do
+  it "加算" do
     five = Tdd::Money.dollar(5)
     sum = five.plus(Tdd::Money.dollar(5))
     bank = Tdd::Bank.new
@@ -32,26 +29,26 @@ RSpec.describe "Money Test" do # rubocop:disable RSpec/DescribeClass
     expect(reduced).to eql(Tdd::Money.dollar(10))
   end
 
-  it "test reduce sum" do
+  it "reduceメソッドによる計算" do
     sum = Tdd::Sum.new(Tdd::Money.dollar(3), Tdd::Money.dollar(4))
     bank = Tdd::Bank.new
     result = bank.reduce(sum, "USD")
     expect(result).to eql(Tdd::Money.dollar(7))
   end
 
-  it "test reduce money different currency" do
+  it "異なる通貨へ換算" do
     bank = Tdd::Bank.new
     bank.add_rate("CHF", "USD", 2)
     result = bank.reduce(Tdd::Money.franc(2), "USD")
     expect(result).to eql(Tdd::Money.dollar(1))
   end
 
-  it "test identity rate" do
+  it "通貨の計算にてA国からA国の通貨への変換レートの確認" do
     bank = Tdd::Bank.new
     expect(bank.rate("USD", "USD")).to eq(1)
   end
 
-  it "test mixed addition" do
+  it "異なる通貨同士の加算" do
     five_bucks = Tdd::Money.dollar(5)
     ten_francs = Tdd::Money.franc(10)
     bank = Tdd::Bank.new
@@ -60,7 +57,7 @@ RSpec.describe "Money Test" do # rubocop:disable RSpec/DescribeClass
     expect(result).to eql(Tdd::Money.dollar(10))
   end
 
-  it "test sum plus money" do
+  it "Sumクラスの加算" do
     five_bucks = Tdd::Money.dollar(5)
     ten_francs = Tdd::Money.franc(10)
     bank = Tdd::Bank.new
@@ -70,7 +67,7 @@ RSpec.describe "Money Test" do # rubocop:disable RSpec/DescribeClass
     expect(result).to eql(Tdd::Money.dollar(15))
   end
 
-  it "test sum times" do
+  it "Sumクラスの乗算" do
     five_bucks = Tdd::Money.dollar(5)
     ten_francs = Tdd::Money.franc(10)
     bank = Tdd::Bank.new
