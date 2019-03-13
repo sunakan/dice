099db9e37863e175eadcda27b24da180815c6d26
sunakan <sunakan03@gmail.com>
2019-03-13 21:24:17 +0900

    [Add] #56 setupを最初に呼び出す

--- lib/tdd/xunit.rb
+++ lib/tdd/xunit.rb
@@ -6,29 +6,41 @@ class TestCase
   end
 
   def run
-    self.send(@name)
+    setup()
+    send(@name)
   end
 end
 
 class WasRun < TestCase
-  attr_reader :was_run
-  def initialize(name)
-    super(name)
-    @was_run = nil
+  attr_reader :was_run, :was_setup
+  def setup
+    @was_run   = nil
+    @was_setup = 1
   end
-
   def test_method
     @was_run = 1
   end
 end
 
 class TestCaseTest < TestCase
+  private
+    attr_accessor :test
+  public
   def test_running
     test = WasRun.new("test_method")
-    raise unless test.was_run.nil?
-    test.run
+    test.run()
     raise if test.was_run.nil?
+  end
+
+  def test_setup
+    @test.run()
+    raise if test.was_setup.nil?
+  end
+
+  def setup
+    @test = WasRun.new("test_method")
   end
 end
 
 TestCaseTest.new("test_running").run()
+TestCaseTest.new("test_setup").run()
