fdc8983368e677611776c9a6274350ba4dda34da
sunakan <sunakan03@gmail.com>
2019-03-14 21:49:32 +0900

    [Add] #59 収集したテスト結果を出力する
    - raise Exceptionをキャッチできてないので一旦棚上げ

--- lib/tdd/xunit.rb
+++ lib/tdd/xunit.rb
@@ -6,9 +6,12 @@ class TestCase
   end
 
   def run
+    result = TestResult.new()
+    result.test_started()
     setup()
     send(@name)
     tear_down()
+    return result
   end
 
   def setup
@@ -26,6 +29,9 @@ class WasRun < TestCase
   def test_method
     @log = "#{@log} #{__method__.to_s}"
   end
+  def test_broken_method
+    raise Exception
+  end
   def tear_down
     @log = "#{@log} #{__method__.to_s}"
   end
@@ -39,6 +45,37 @@ class TestCaseTest < TestCase
     @test.run()
     raise unless @test.log == "setup test_method tear_down"
   end
+
+  def test_result
+    @test = WasRun.new("test_method")
+    result = @test.run()
+    raise unless result.summary() == "1 run, 0 failed"
+  end
+
+  def test_failed_result
+    @test = WasRun.new("test_broken_method")
+    result = @test.run()
+    raise unless result.summary() == "1 run, 1 failed"
+  end
+end
+
+class TestResult
+  private
+    attr_accessor :run_count
+  public
+  def initialize
+    @run_count = 0
+  end
+
+  def test_started
+    @run_count += 1
+  end
+
+  def summary
+    "#{@run_count} run, 0 failed"
+  end
 end
 
 TestCaseTest.new("test_template_method").run()
+TestCaseTest.new("test_result").run()
+#TestCaseTest.new("test_failed_result").run()
