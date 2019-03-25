6289dbf9f1395c6a0c4fb7e64434540cf05bcb0e
sunakan <sunakan03@gmail.com>
2019-03-25 20:26:37 +0900

    [Clean] rubocop -R

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :truncation
     DatabaseCleaner[:active_record].strategy = :truncation
-     DatabaseCleaner[:active_record, model: Paiza::AppRecord].strategy = :truncation
+    DatabaseCleaner[:active_record, model: Paiza::AppRecord].strategy = :truncation
     # DatabaseCleaner[:active_record, model: Worldcup2014::AppRecord].strategy = :truncation
     # DatabaseCleaner[:active_record, model: EverydayRailsRspec::AppRecord].strategy = :truncation
     load(Rails.root.join("db", "worldcup2014", "seeds.rb"))
